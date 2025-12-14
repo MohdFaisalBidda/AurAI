0ffba25aed1a02dc309a20b682771d95195d2d0f
MohdFaisalBidda <biddafaisal@gmail.com>
2025-12-14 17:30:04 +0530

    Update sendEmail.ts

--- backend/utils/sendEmail.ts
+++ backend/utils/sendEmail.ts
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 export async function sendEmail(to: string, subject: string, body: string) {
     let transporter = nodemailer.createTransport({
         host: "smtp-relay.brevo.com",
-        port: 587,
+        port: process.env.SMTP_PORT,
         secure: false,
         auth: {
             user: process.env.BREVO_SMTP_USER,
